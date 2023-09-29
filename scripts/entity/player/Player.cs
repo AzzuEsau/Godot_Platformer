96c5df81289f922ae17ab31592bacf554b490a20
AzzuEsau <andres.lopez4191@alumnos.udg.mx>
2023-09-29 11:49:49 -0600

    starting to use tilemaps and player movement

---
+++ scripts/entity/player/Player.cs
@@ -0,0 +1,59 @@
+using Godot;
+using System;
+
+public partial  class Player : CharacterBody2D {
+	#region Variables
+		#region Exports
+			[Export] private AnimationPlayer animator;
+			[Export] private Sprite2D sprite;
+		#endregion
+
+		private float direction;
+		private float speed = 200;
+		private float jumpSpeed = 250;
+		private bool isJumping;
+	#endregion
+
+	#region Signals
+		// [Signal] public delegate void ExampleSignalEventHandler();
+	#endregion
+
+	#region Godot Methdos
+		public override void _Ready() {
+			
+		}
+
+		public override void _Process(double delta) {
+			AnimatePlayer();
+		}
+
+		public override void _PhysicsProcess(double delta) {
+			ReadInput();
+			MovePlayer();
+		}
+    #endregion
+
+    #region My Methods
+		private void ReadInput() {
+			direction = Input.GetAxis(GameResources.KeyMoveLeft, GameResources.KeyMoveRight);
+			isJumping = Input.IsActionJustPressed(GameResources.KeyJump) && IsOnFloor();
+		}
+
+		private void MovePlayer() {
+			float ySpeed = isJumping ? Velocity.Y - jumpSpeed : Velocity.Y + GameResources.Gravity;
+			Velocity = new Vector2(direction * speed, ySpeed);
+
+			MoveAndSlide();
+		}
+
+		private void AnimatePlayer() {
+			if(direction != 0) animator.Play("walk");
+			else animator.Play("idle");
+
+			if(direction != 0) sprite.FlipH = direction < 0;
+		}
+    #endregion
+
+    #region Events
+    #endregion
+}
---
+++ scripts/resources/GameResources.cs
@@ -0,0 +1,14 @@
+using Godot;
+using System;
+
+public static class GameResources {
+    #region Inputs
+        public static string KeyMoveLeft = "move_left";
+        public static string KeyMoveRight = "move_right";
+        public static string KeyJump = "jump";
+    #endregion
+
+    #region Physiscs
+        public static float Gravity = 9.8F;
+    #endregion
+}
